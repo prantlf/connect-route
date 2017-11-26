14401180b8ada010ceb79db39d590ce8957b1bd1
Ferdinand Prantl <prantlf@gmail.com>
2017-11-26 11:33:07 +0100

    chore: Apply JavaScript (Coding) Standard

--- index.js
+++ index.js
@@ -1,1 +1,179 @@
-module.exports = require('./lib/connect-route');
+'use strict'
+
+var methods = [
+  'get', 'post', 'put', 'head', 'patch', 'delete', 'connect', 'options',
+  'trace', 'copy', 'lock', 'mkcol', 'move', 'propfind', 'proppatch',
+  'unlock', 'report', 'mkactivity', 'checkout', 'merge'
+]
+var separator = /^[\s/]+|[\s/]+$/g
+var i, length
+
+function chainHandlers (chain) {
+  return function (request, response, next) {
+    var handlers = chain.slice()
+    var handle = function () {
+      var handler = handlers.shift()
+      if (handler) {
+        handler(request, response, handle)
+      } else {
+        next()
+      }
+    }
+    handle()
+  }
+}
+
+function createMethodHandler (method) {
+  method = method.toUpperCase()
+  return function () {
+    var routes = []
+    var handlers = []
+    var i, handler
+    for (i = 0; i < arguments.length; ++i) {
+      if (typeof arguments[i] === 'function') {
+        handlers.push(arguments[i])
+      } else {
+        routes.push(arguments[i])
+      }
+    }
+    for (i = 0; i < routes.length; ++i) {
+      if (handlers.length > 1) {
+        handler = chainHandlers(handlers)
+      } else {
+        handler = handlers[0]
+      }
+      this.add(method, routes[i], handler)
+    }
+    return this
+  }
+}
+
+var Router = function () {
+  this.routes = {}
+}
+
+Router.prototype.add = function (method, route, handler) {
+  var parts, part, current, i, length, name, prefix
+
+  if (typeof handler !== 'function') {
+    return
+  }
+  if (!this.routes[method]) {
+    this.routes[method] = {
+      childs: {} // handler: undefined, route: undefined
+    }
+  }
+
+  parts = route.split('?', 1)[0].replace(separator, '').split('/')
+
+  if (!parts[0].length) {
+    this.routes[method].handler = handler
+    this.routes[method].route = route
+  } else {
+    current = this.routes[method]
+    for (i = 0, length = parts.length; i < length; ++i) {
+      part = parts[i]
+      name = undefined
+      prefix = part.charAt(0)
+      if (prefix === ':' || prefix === '*') {
+        name = part.substr(1)
+        part = parts[i] = '*'
+      }
+      if ((!current.childs[part] && (current.childs[part] = {})) ||
+           (part === '*' && !current.childs[part][length])) {
+        if (part === '*') {
+          current.childs[part][length] = {
+            childs: {}, name: name // handler: undefined, route: undefined
+          }
+        } else {
+          current.childs[part] = {
+            childs: {}, name: name // handler: undefined, route: undefined
+          }
+        }
+      }
+      if (part === '*') {
+        current = current.childs[part][length]
+        if (prefix === '*') {
+          current.last = true
+          break
+        }
+      } else {
+        current = current.childs[part]
+      }
+    }
+    current.handler = handler
+    current.route = route
+  }
+}
+
+Router.prototype.match = function (method, url) {
+  var parts = decodeURI(url).split('?', 1)[0]
+                            .replace(separator, '')
+                            .split('/')
+  var result = {
+    params: {} // handler: undefined, route: undefined
+  }
+  var part, child, current, i, length
+
+  if (this.routes[method]) {
+    if (!parts[0].length) {
+      result.handler = this.routes[method].handler
+      result.route = this.routes[method].route
+    } else {
+      current = this.routes[method]
+      for (i = 0, length = parts.length; i < length; ++i) {
+        part = parts[i]
+        if (current.childs[part]) {
+          current = current.childs[part]
+          result.handler = current.handler
+          result.route = current.route
+        } else {
+          child = current.childs['*']
+          if (child && child[i + 1] && child[i + 1].last) {
+            current = child[i + 1]
+            result.handler = current.handler
+            result.route = current.route
+            result.params[current.name] = parts.slice(i)
+            break
+          } else if (child && child[length]) {
+            current = child[length]
+            result.handler = current.handler
+            result.route = current.route
+            result.params[current.name] = part
+          } else {
+            result.handler = undefined
+            result.route = undefined
+            result.params = {}
+            break
+          }
+        }
+      }
+    }
+  }
+  return result
+}
+
+for (i = 0, length = methods.length; i < length; ++i) {
+  var method = methods[i]
+  Router.prototype[method] = createMethodHandler(method)
+}
+
+module.exports = function (cb) {
+  var router = new Router()
+  if (typeof cb === 'function') {
+    cb(router)
+  }
+
+  return function (req, res, next) {
+    var action = router.match(req.method, req.url)
+    if (action.handler) {
+      req.route = action.route
+      req.params = action.params
+      action.handler(req, res, next)
+    } else {
+      req.route = undefined
+      req.params = {}
+      next()
+    }
+  }
+}
--- test/routes.js
+++ test/routes.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-const expect = require('chai').expect,
-      route = require('../index');
+const expect = require('chai').expect
+const route = require('..')
 
 describe('Router', function () {
   // this.route - has to be initialized by calling route()
@@ -10,26 +10,26 @@ describe('Router', function () {
   // this.next  - allows checking, if next() has been called
   before(function () {
     this.serve = function (method, url) {
-      var req = {
-            method: method,
-            url: url
-          },
-          next = function () {
-            next.called = true;
-          };
-      this.req = req;
-      next.called = false;
-      this.next = next;
-      this.route(req, {}, next);
-    };
-  });
+      const req = {
+        method: method,
+        url: url
+      }
+      const next = function () {
+        next.called = true
+      }
+      this.req = req
+      next.called = false
+      this.next = next
+      this.route(req, {}, next)
+    }
+  })
 
   it('allows chaining of the route-registering methods', function () {
     route(function (router) {
-      var result = router.get('/', function (req, res, next) {});
-      expect(result).to.equal(router);
-    });
-  });
+      const result = router.get('/', function (req, res, next) {})
+      expect(result).to.equal(router)
+    })
+  })
 
   describe('when matching the method', function () {
     it('matches all usual methods', function () {
@@ -39,288 +39,288 @@ describe('Router', function () {
         'PROPPATCH', 'UNLOCK', 'REPORT', 'MKACTIVITY', 'CHECKOUT', 'MERGE'
       ].forEach(function (method) {
         this.route = route(function (router) {
-          var route = router[method.toLowerCase()];
-          expect(route).to.be.a('function');
-          route.call(router, '/', function (req, res, next) {});
-        });
-        this.serve(method, '/');
-        expect(this.next.called).to.equal(false);
-      }, this);
-    });
+          const route = router[method.toLowerCase()]
+          expect(route).to.be.a('function')
+          route.call(router, '/', function (req, res, next) {})
+        })
+        this.serve(method, '/')
+        expect(this.next.called).to.equal(false)
+      }, this)
+    })
 
     it('does not match an invalid method', function () {
       this.route = route(function (router) {
-        router.get('/', function (req, res, next) {});
-      });
-      this.serve('TEST', '/');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/', function (req, res, next) {})
+      })
+      this.serve('TEST', '/')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('matches an upper-case method', function () {
       this.route = route(function (router) {
-        router.get('/', function (req, res, next) {});
-      });
-      this.serve('GET', '/');
-      expect(this.next.called).to.equal(false);
-    });
+        router.get('/', function (req, res, next) {})
+      })
+      this.serve('GET', '/')
+      expect(this.next.called).to.equal(false)
+    })
 
     it('does not match a lower-case method', function () {
       this.route = route(function (router) {
-        router.get('/', function (req, res, next) {});
-      });
-      this.serve('get', '/');
-      expect(this.next.called).to.equal(true);
-    });
-  });
+        router.get('/', function (req, res, next) {})
+      })
+      this.serve('get', '/')
+      expect(this.next.called).to.equal(true)
+    })
+  })
 
   describe('when matching the path', function () {
     it('matches the root path (/) only', function () {
       this.route = route(function (router) {
-        router.get('/', function (req, res, next) {});
-      });
-      this.serve('GET', '/');
-      expect(this.next.called).to.equal(false);
-    });
+        router.get('/', function (req, res, next) {})
+      })
+      this.serve('GET', '/')
+      expect(this.next.called).to.equal(false)
+    })
 
     it('matches using case-sensitive string comparisons', function () {
       this.route = route(function (router) {
-        router.get('/test', function (req, res, next) {});
-      });
-      this.serve('GET', '/TEST');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test', function (req, res, next) {})
+      })
+      this.serve('GET', '/TEST')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('matches a path with a single part (/test) only', function () {
       this.route = route(function (router) {
-        router.get('/test', function (req, res, next) {});
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(false);
-    });
+        router.get('/test', function (req, res, next) {})
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(false)
+    })
 
     it('matches a path with two parts (/test1/test2) only', function () {
       this.route = route(function (router) {
-        router.get('/test1/test2', function (req, res, next) {});
-      });
-      this.serve('GET', '/test1/test2');
-      expect(this.next.called).to.equal(false);
-    });
+        router.get('/test1/test2', function (req, res, next) {})
+      })
+      this.serve('GET', '/test1/test2')
+      expect(this.next.called).to.equal(false)
+    })
 
     it('does not match an extra part at the beginning of the path (/test)', function () {
       this.route = route(function (router) {
-        router.get('/test', function (req, res, next) {});
-      });
-      this.serve('GET', '/start/test');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test', function (req, res, next) {})
+      })
+      this.serve('GET', '/start/test')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('does not match an extra part at the end of the path (/test)', function () {
       this.route = route(function (router) {
-        router.get('/test', function (req, res, next) {});
-      });
-      this.serve('GET', '/test/end');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test', function (req, res, next) {})
+      })
+      this.serve('GET', '/test/end')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('does not match a missing part at the end of the path (/test1/test2)', function () {
       this.route = route(function (router) {
-        router.get('/test1/test2', function (req, res, next) {});
-      });
-      this.serve('GET', '/test1');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test1/test2', function (req, res, next) {})
+      })
+      this.serve('GET', '/test1')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('does not match a missing part at the beginning of the path (/test1/test2)', function () {
       this.route = route(function (router) {
-        router.get('/test1/test2', function (req, res, next) {});
-      });
-      this.serve('GET', '/test2');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test1/test2', function (req, res, next) {})
+      })
+      this.serve('GET', '/test2')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('matches a parametrized part at the path beginning (/:id)', function () {
       this.route = route(function (router) {
-        router.get('/:id', function (req, res, next) {});
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.id).to.equal('test');
-    });
+        router.get('/:id', function (req, res, next) {})
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.id).to.equal('test')
+    })
 
     it('matches a parametrized part at the path end (/start/:id)', function () {
       this.route = route(function (router) {
-        router.get('/start/:id', function (req, res, next) {});
-      });
-      this.serve('GET', '/start/test');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.id).to.equal('test');
-    });
+        router.get('/start/:id', function (req, res, next) {})
+      })
+      this.serve('GET', '/start/test')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.id).to.equal('test')
+    })
 
     it('matches a parametrized part in the middle of the path (/start/:id/end)', function () {
       this.route = route(function (router) {
-        router.get('/start/:id/end', function (req, res, next) {});
-      });
-      this.serve('GET', '/start/test/end');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.id).to.equal('test');
-    });
+        router.get('/start/:id/end', function (req, res, next) {})
+      })
+      this.serve('GET', '/start/test/end')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.id).to.equal('test')
+    })
 
     it('matches two parametrized parts of the path (/:id/:type)', function () {
       this.route = route(function (router) {
-        router.get('/:id/:type', function (req, res, next) {});
-      });
-      this.serve('GET', '/test1/test2');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.id).to.equal('test1');
-      expect(this.req.params.type).to.equal('test2');
-    });
+        router.get('/:id/:type', function (req, res, next) {})
+      })
+      this.serve('GET', '/test1/test2')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.id).to.equal('test1')
+      expect(this.req.params.type).to.equal('test2')
+    })
 
     it('matches the whole path as a parametrized part (/*path)', function () {
       this.route = route(function (router) {
-        router.get('/*path', function (req, res, next) {});
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.path).to.deep.equal(['test']);
-    });
+        router.get('/*path', function (req, res, next) {})
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.path).to.deep.equal(['test'])
+    })
 
     it('matches the rest of the path as a parametrized part (/start/*path)', function () {
       this.route = route(function (router) {
-        router.get('/start/*path', function (req, res, next) {});
-      });
-      this.serve('GET', '/start/test');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.path).to.deep.equal(['test']);
-    });
+        router.get('/start/*path', function (req, res, next) {})
+      })
+      this.serve('GET', '/start/test')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.path).to.deep.equal(['test'])
+    })
 
     it('returns the rest of the path parts split (/*path)', function () {
       this.route = route(function (router) {
-        router.get('/*path', function (req, res, next) {});
-      });
-      this.serve('GET', '/test1/test2');
-      expect(this.next.called).to.equal(false);
-      expect(this.req.params.path).to.deep.equal(['test1', 'test2']);
-    });
-  });
+        router.get('/*path', function (req, res, next) {})
+      })
+      this.serve('GET', '/test1/test2')
+      expect(this.next.called).to.equal(false)
+      expect(this.req.params.path).to.deep.equal(['test1', 'test2'])
+    })
+  })
 
   describe('when using multiple routes or handlers', function () {
     it('executes the handler, if either of two paths matches', function () {
       this.route = route(function (router) {
-        router.get('/test1', '/test2', function (req, res, next) {});
-      });
-      this.serve('GET', '/test1');
-      expect(this.next.called).to.equal(false);
-      this.serve('GET', '/test2');
-      expect(this.next.called).to.equal(false);
-    });
+        router.get('/test1', '/test2', function (req, res, next) {})
+      })
+      this.serve('GET', '/test1')
+      expect(this.next.called).to.equal(false)
+      this.serve('GET', '/test2')
+      expect(this.next.called).to.equal(false)
+    })
 
     it('executes next(), if neither of two paths matches', function () {
       this.route = route(function (router) {
-        router.get('/test1', '/test2', function (req, res, next) {});
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test1', '/test2', function (req, res, next) {})
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('executes two chained handlers, if the path matches', function () {
-      var first, second;
+      var first, second
       this.route = route(function (router) {
         router.get('/test',
           function (req, res, next) {
-            first = true;
-            next();
+            first = true
+            next()
           }, function (req, res, next) {
-            second = true;
-          });
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(false);
-      expect(first).to.equal(true);
-      expect(second).to.equal(true);
-    });
+            second = true
+          })
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(false)
+      expect(first).to.equal(true)
+      expect(second).to.equal(true)
+    })
 
     it('executes two chained handlers twice, if both paths match', function () {
-      var first = 0,
-          second = 0;
+      var first = 0
+      var second = 0
       this.route = route(function (router) {
         router.get('/test1', '/test2',
           function (req, res, next) {
-            ++first;
-            next();
+            ++first
+            next()
           }, function (req, res, next) {
-            ++second;
-          });
-      });
-      this.serve('GET', '/test1');
-      expect(this.next.called).to.equal(false);
-      expect(first).to.equal(1);
-      expect(second).to.equal(1);
-      this.serve('GET', '/test2');
-      expect(this.next.called).to.equal(false);
-      expect(first).to.equal(2);
-      expect(second).to.equal(2);
-    });
+            ++second
+          })
+      })
+      this.serve('GET', '/test1')
+      expect(this.next.called).to.equal(false)
+      expect(first).to.equal(1)
+      expect(second).to.equal(1)
+      this.serve('GET', '/test2')
+      expect(this.next.called).to.equal(false)
+      expect(first).to.equal(2)
+      expect(second).to.equal(2)
+    })
 
     it('executes the first of two non-chained handlers, if the path matches', function () {
-      var first, second;
+      var first, second
       this.route = route(function (router) {
         router.get('/test',
           function (req, res, next) {
-            first = true;
+            first = true
           }, function (req, res, next) {
-            second = true;
-          });
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(false);
-      expect(first).to.equal(true);
-      expect(second).to.equal(undefined);
-    });
+            second = true
+          })
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(false)
+      expect(first).to.equal(true)
+      expect(second).to.equal(undefined)
+    })
 
     it('executes the first of two non-chained handlers twice, if the path matches', function () {
-      var first = 0,
-          second = 0;
+      var first = 0
+      var second = 0
       this.route = route(function (router) {
         router.get('/test1', '/test2',
           function (req, res, next) {
-            ++first;
+            ++first
           }, function (req, res, next) {
-            ++second;
-          });
-      });
-      this.serve('GET', '/test1');
-      expect(this.next.called).to.equal(false);
-      expect(first).to.equal(1);
-      expect(second).to.equal(0);
-      this.serve('GET', '/test2');
-      expect(this.next.called).to.equal(false);
-      expect(first).to.equal(2);
-      expect(second).to.equal(0);
-    });
+            ++second
+          })
+      })
+      this.serve('GET', '/test1')
+      expect(this.next.called).to.equal(false)
+      expect(first).to.equal(1)
+      expect(second).to.equal(0)
+      this.serve('GET', '/test2')
+      expect(this.next.called).to.equal(false)
+      expect(first).to.equal(2)
+      expect(second).to.equal(0)
+    })
 
     it('executes next(), if neither of two paths matches', function () {
       this.route = route(function (router) {
-        router.get('/test1', '/test2', function (req, res, next) {});
-      });
-      this.serve('GET', '/test');
-      expect(this.next.called).to.equal(true);
-    });
+        router.get('/test1', '/test2', function (req, res, next) {})
+      })
+      this.serve('GET', '/test')
+      expect(this.next.called).to.equal(true)
+    })
 
     it('executes next(), if the path does not match', function () {
-      var first, second;
+      var first, second
       this.route = route(function (router) {
         router.get('/test',
           function (req, res, next) {
-            first = true;
-            next();
+            first = true
+            next()
           }, function (req, res, next) {
-            second = true;
-          });
-      });
-      this.serve('GET', '/test1');
-      expect(this.next.called).to.equal(true);
-      expect(first).to.equal(undefined);
-      expect(second).to.equal(undefined);
-    });
-  });
-});
+            second = true
+          })
+      })
+      this.serve('GET', '/test1')
+      expect(this.next.called).to.equal(true)
+      expect(first).to.equal(undefined)
+      expect(second).to.equal(undefined)
+    })
+  })
+})
